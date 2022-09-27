418f648ef16250fe3cfab2a6f6ca6a41bca1b312
Géssica Mendonça <gm.gessica@outlook.com>
2022-09-27 17:14:07 -0300

    consumindo api

--- src/pages/CepSearch/index.tsx
+++ src/pages/CepSearch/index.tsx
@@ -1,29 +1,83 @@
 import './styles.css';
 
 import ResultCard from 'components/ResultCard';
+import { useState } from 'react';
+import axios from 'axios';
+
+type FormData = {
+  cep: string;
+};
+
+type Address = {
+  cep: string;
+  logradouro: string;
+  localidade: string;
+  complemento: string;
+  bairro: string;
+  uf: string;
+  ibge: string;
+  gia: string;
+  ddd: string;
+  siafi: string;
+};
 
 const CepSearch = () => {
+  const [address, setAddress] = useState<Address>();
+
+  const [formData, setFormData] = useState<FormData>({ cep: '' });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    axios
+      .get(`https://viacep.com.br/ws/${formData.cep}/json`)
+      .then((response) => {
+        setAddress(response.data);
+      })
+      .catch((error) => {
+        setAddress(undefined);
+      });
+  };
+
   return (
     <div className="cep-search-container">
       <h1 className="text-primary">Busca CEP</h1>
       <div className="container search-container">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-container">
             <input
               type="text"
+              name="cep"
+              value={formData.cep}
               className="search-input"
               placeholder="CEP (somente números)"
-              onChange={() => {}}
+              onChange={handleChange}
             />
+
             <button type="submit" className="btn btn-primary search-button">
               Buscar
             </button>
           </div>
         </form>
-
-        <ResultCard title="Logradouro" description="Lalala" />
-        <ResultCard title="Número" description="234" />
-
+        {address && (
+          <>
+            <ResultCard title="Cep" description={address.cep} />
+            <ResultCard title="Logradouro" description={address.logradouro} />
+            <ResultCard title="Localidade" description={address.localidade} />
+            <ResultCard title="Bairro" description={address.bairro} />
+            <ResultCard title="UF" description={address.uf} />
+            <ResultCard title="IBGE" description={address.ibge} />
+            <ResultCard title="Gia" description={address.gia} />
+            <ResultCard title="DDD" description={address.ddd} />
+            <ResultCard title="Siafi" description={address.siafi} />
+          </>
+        )}
       </div>
     </div>
   );
